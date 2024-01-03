21b8f8b1eff3026817fed2636cb055f89a22bd96
Oscar <oscarchase@gmail.com>
2024-01-02 20:53:29 -0800

    Add util trait for inspecting only in debug build

--- solutions/src/lib.rs
+++ solutions/src/lib.rs
@@ -1,1 +1,19 @@
-pub mod prelude {}
+pub mod prelude {
+    pub trait DebugInspect: Iterator {
+        fn debug_inspect<F>(self, f: F) -> std::iter::Inspect<Self, impl FnMut(&Self::Item)>
+            where
+                Self: Sized,
+                F: FnMut(&Self::Item)
+        {
+            #[cfg(debug_assertions)]
+            let out = self.inspect(f);
+    
+            #[cfg(not(debug_assertions))]
+            let out = self.inspect(|_|{});
+    
+            out
+        }
+    }
+    
+    impl<I: Iterator> DebugInspect for I {}
+}
